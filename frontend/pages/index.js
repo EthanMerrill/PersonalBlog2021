05ad64d364905f157c618544c5650ed66411a6d5
Ethan Merrill <eth22mer@gmail.com>
2021-01-01 13:33:24 -0500

    somewhat working d3

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,31 @@
 import React from "react";
+import Head from 'next/head'
 import Articles from "../components/articles";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import ForceDirectedNav from "../components/ForceDirectedNav";
 import { fetchAPI } from "../lib/api";
+import graphData from "../assets/graphData"
+
 
 const Home = ({ articles, categories, homepage }) => {
+
   return (
+    <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+
+        <html xmlnsXlinkk="http://www.w3.org/1999/xlink"></html>
+        <script src="https://d3js.org/d3.v6.min.js"></script>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></link>
+
+        <script src="https://kit.fontawesome.com/374cfc1460.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" ></link>
+      </Head>
     <Layout categories={categories}>
       <Seo seo={homepage.seo} />
+        <ForceDirectedNav data={graphData} />
       <div className="uk-section">
         <div className="uk-container uk-container-large">
           <h1>{homepage.hero.title}</h1>
@@ -15,21 +33,22 @@ const Home = ({ articles, categories, homepage }) => {
         </div>
       </div>
     </Layout>
+    </div >
   );
 };
 
 export async function getStaticProps() {
-  // Run API calls in parallel
-  const [articles, categories, homepage] = await Promise.all([
-    fetchAPI("/articles?status=published"),
-    fetchAPI("/categories"),
-    fetchAPI("/homepage"),
-  ]);
+    // Run API calls in parallel
+    const [articles, categories, homepage] = await Promise.all([
+        fetchAPI("/articles?status=published"),
+        fetchAPI("/categories"),
+        fetchAPI("/homepage"),
+    ]);
 
-  return {
-    props: { articles, categories, homepage },
-    revalidate: 1,
-  };
+    return {
+        props: { articles, categories, homepage },
+        revalidate: 1,
+    };
 }
 
 export default Home;
